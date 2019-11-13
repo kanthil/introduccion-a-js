9dd782498ccf6f999d564b25010bdce6ecaa7aae
Brian R <brianmr22@gmail.com>
2019-11-13 17:08:05 -0300

    Agrega tarea 6

--- tareas/clase-6/tarea-clase-6.js
+++ tareas/clase-6/tarea-clase-6.js
@@ -7,6 +7,62 @@ Punto bonus: Crear un botón para "empezar de nuevo" que empiece el proceso nuev
 */
 
 
+const $botonConfirmar = document.querySelector('#confirmar-familiares');
+$botonConfirmar.onclick = function(){
+    let cantidadFamilares = Number(document.querySelector('#familiares').value);
+    let nodoBody = document.querySelector('body');
+
+    let nuevoParrafo = document.createElement('p');
+    let textoParrafo = document.createTextNode('Ingrese la edad de sus familiares:');
+    nuevoParrafo.appendChild(textoParrafo);
+    nodoBody.appendChild(nuevoParrafo);
+
+    //Crea div contenedor de los inputs
+    let nuevoDivInputs = document.createElement('div');
+    nuevoDivInputs.id = "div-inputs";
+    nodoBody.appendChild(nuevoDivInputs);
+
+    //Crea boton calcular edad
+    let nuevoBotonConfirmar = document.createElement('button');
+    let textoBotonConfirmar = document.createTextNode('Calcular');
+    nuevoBotonConfirmar.id = 'calcular-edades';
+    nuevoBotonConfirmar.appendChild(textoBotonConfirmar);
+    nodoBody.appendChild(nuevoBotonConfirmar);
+
+    //Loop que crea inputs en base a cantidadFamiliares
+    for (i = 0; i < cantidadFamilares; i++){
+        let nuevosInputs = document.createElement('input');
+        let lineBreak = document.createElement('br');
+        nuevosInputs.placeholder = `Edad familiar #${i + 1}`;
+        nuevosInputs.className = 'edad-inputs';        
+        nuevoDivInputs.appendChild(nuevosInputs);
+        nuevoDivInputs.appendChild(lineBreak);
+    }
+
+	//Boton confirmar
+    nuevoBotonConfirmar.onclick = function(){
+        let valuesInputs = document.querySelectorAll('.edad-inputs');
+        let mayorEdad = Number(valuesInputs[0].value);
+        let menorEdad = Number(valuesInputs[0].value);
+        let promedioEdad = 0;
+        for (i = 0; i < valuesInputs.length; i++){
+            if (valuesInputs[i].value > mayorEdad){
+                mayorEdad = valuesInputs[i].value;
+            }
+            if (valuesInputs[i].value < menorEdad){
+                menorEdad = valuesInputs[i].value;
+            }
+            promedioEdad += Number(valuesInputs[i].value);
+        }
+        
+        promedioEdad /= valuesInputs.length;
+        let calculosEdad = document.createElement('p');
+        let calculosEdadTexto = document.createTextNode(`La mayor edad es de ${mayorEdad} años. La menor edad es de ${menorEdad} años. La edad promedio es de ${promedioEdad} años.`);
+        calculosEdad.appendChild(calculosEdadTexto);
+        nodoBody.appendChild(calculosEdadTexto);
+    }
+}
+
 /*
 TAREA:
 Crear una interfaz que permita agregar ó quitar (botones agregar y quitar) inputs+labels para completar el salario anual de cada integrante de la familia que trabaje.
